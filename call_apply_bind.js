1d4fe0824d899d5f13182190effd04add9acf278
Laxmikant04 <Laxmikant04@users.noreply.github.com>
2019-02-03 11:48:43 +0530

    js concept

---
+++ call_apply_bind.js
@@ -0,0 +1,28 @@
+/**
+ * call apply and bind methods are used to execute a functions from different scopes Or over different ojects.
+ 
+ * Difference-
+    Call accepts parameters in form of comma seprated arguments.
+    Apply accepts parameters as an array of arguments.
+
+    Bind accepts parameters as comma seprated arguments and it returns a refrence to function which can be executed later
+    where both Call and Apply gets executed as soon as called.
+ */
+
+
+var obj={
+    num : 2
+ }
+ var add = function(num2,num3,num4){
+    return this.num + num2 + num3 + num4;
+ }
+ var arr=[3,4,5];
+ //Call method 
+ console.log(add.call(obj,3,4,5));  //Output : 14 //params as comma seprated and code executes as soon as called
+ //Apply method
+ console.log(add.apply(obj,arr));   //Output : 14 //params as array and code executes as soon as called
+ //bind Method
+ var bound = add.bind(obj);                       ////params as comma seprated and returns a refrence fuction
+ console.log(bound(3,4,5));         //output : 14 
+
+ 
---
+++ eS6.js
@@ -0,0 +1,11 @@
+/**
+ * Importent New features
+ * 
+ * Let and Const keyword - block scope
+ * Array functions - foreach,map,filter , reduce
+ * arrow functions
+ * Classes - inheritance with extends
+ * Improved object litrals - Properties and functions
+ * template strings
+ * Promises/async await
+ */
