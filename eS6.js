3fdd26e340cdc3ea7ea822899f8459070de091ae
Laxmikant <45842339+Laxmikant04@users.noreply.github.com>
2020-04-28 22:23:45 +0530

    Update eS6.js

--- eS6.js
+++ eS6.js
@@ -48,6 +48,25 @@ rest / spread properties:
   function f(a, b, ...theArgs) {
   // ...
   }
-
   
+  Spread syntax allows an iterable such as an array expression or string to be expanded in places where zero or more arguments (for function calls)
+  or elements (for array literals) are expected.
+  
+  Syntax
+  For function calls:
+  myFunction(...iterableObj);
 
+  For array literals or strings:
+  [...iterableObj, '4', 'five', 6];
+
+  For object literals (new in ECMAScript 2018):
+  let objClone = { ...obj };
+
+  Eg:-
+  function sum(x, y, z) {
+    return x + y + z;
+  }
+  const numbers = [1, 2, 3];
+  console.log(sum(...numbers));
+  // expected output: 6
+  
