19eff3eb431b891cfae54112823c185acae6714f
Laxmikant <45842339+Laxmikant04@users.noreply.github.com>
2020-04-28 19:43:37 +0530

    Update eS6.js

--- eS6.js
+++ eS6.js
@@ -2,6 +2,7 @@
  * Importent New features
  * 
  * Let and Const keyword - block scope
+ * Added default parameter values.
  * Array functions - foreach,map,filter , reduce
  * arrow functions
  * Classes - inheritance with extends
