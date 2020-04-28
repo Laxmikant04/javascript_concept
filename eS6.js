b95dd07d29d11a0a5048be02dc71e6146c4b802d
Laxmikant <45842339+Laxmikant04@users.noreply.github.com>
2020-04-28 20:56:25 +0530

    Update eS6.js

--- eS6.js
+++ eS6.js
@@ -25,5 +25,29 @@ Shared memory concept :
   point the original data and access/update the same.
   more info - https://www.hongkiat.com/blog/shared-memory-in-javascript/
   
-padding concept
-rest / spread properties
+string padding concept:
+  1)padStart -
+  The padStart() method pads the current string with another string (multiple times, if needed) until the resulting string reaches the given length.
+  The padding is applied from the start of the current string.
+  
+  Eg: const str1 = '5';
+    console.log(str1.padStart(2, '0'));
+    // expected output: "05"
+
+  2)padEnd-
+  The padEnd() method pads the current string with a given string (repeated, if needed) so that the resulting string reaches a given length.
+  The padding is applied from the end of the current string.
+  
+  Eg:
+  const str2 = '200';
+  console.log(str2.padEnd(5));
+  // expected output: "200  "
+
+rest / spread properties:
+  rest parameter ( three dot ...para) : The rest parameter syntax allows us to represent an indefinite number of arguments as an array.
+  function f(a, b, ...theArgs) {
+  // ...
+  }
+
+  
+
