3d2c085d8a89e6322f7c3dd891ec8bb4f19e6c97
Laxmikant <45842339+Laxmikant04@users.noreply.github.com>
2020-04-28 20:29:43 +0530

    Update eS6.js

--- eS6.js
+++ eS6.js
@@ -17,6 +17,13 @@ https://www.w3schools.com/js/js_versions.asp
 Some new Feature in next versions= 
 Iterator/generator
 Promise.finally()
-Shared memory concept
+Shared memory concept :
+  Shared memory allows javascript threads to access and update same shared memory data. Hence all thread will always see same data.
+  Without shared memory concept , thread shares the data as the copy of original data so every thread will have it's own copy and
+  will access and update the same.
+  With shared memory concept ,thread shares a copy SharedArrayBuffer object which points to the original data so every thread will
+  point the original data and access/update the same.
+  more info - https://www.hongkiat.com/blog/shared-memory-in-javascript/
+  
 padding concept
 rest / spread properties
